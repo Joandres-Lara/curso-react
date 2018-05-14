0f7e616ea77c7ed56efaf9d9d54307493eba8228
Joan Andrés Lara Mora <38198210+Joandres-Lara@users.noreply.github.com>
2018-05-14 15:59:20 -0500

    Update Navbar.js

--- Containers/Navbar.js
+++ Containers/Navbar.js
@@ -3,12 +3,12 @@ import { connect } from 'react-redux'
 import { selectComponentNavbar } from 'actions'
 
 const mapStateToProps = ({config}) => ({
-	items: config.itemsNav,
-	itemActive: config.seccionTarget
+ items: config.itemsNav,
+ itemActive: config.seccionTarget
 })
 
 const mapStateToDispatch = dispatch => ({
-	handleActiveItem: (i) => dispatch( selectComponentNavbar(i) ) 
+ handleActiveItem: (i) => dispatch( selectComponentNavbar(i) ) 
 })
 
 
